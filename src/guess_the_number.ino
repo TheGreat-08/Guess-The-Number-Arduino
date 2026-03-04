d75136440703c00be4b077a7ccbf9f68436865b1
TheGreat-08 <133080802+TheGreat-08@users.noreply.github.com>
2026-03-04 21:54:46 +0530

    Create guess_the_number.ino

---
+++ src/guess_the_number.ino
@@ -0,0 +1,38 @@
+// initialization 
+#include <Keypad.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x20, 16, 2);
+
+// keypad init
+const byte ROWS = 4;
+const byte COLS = 4;
+
+char keys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+
+byte rowPins[ROWS] = {9, 8, 7, 6};
+byte colPins[COLS] = {5, 4, 3, 2};
+
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+
+// LED pins
+#define GREEN 11
+#define RED 12
+#define ORANGE 13
+
+// variables
+String inputNumber = "";
+int secretNumber = 0;
+int tries = 5;
+
+bool gameStarted = false;
+bool waitingForUser1 = false;
+bool waitingForGuess = false;
+bool gameEnded = false;
+char currentMode = ' ';
